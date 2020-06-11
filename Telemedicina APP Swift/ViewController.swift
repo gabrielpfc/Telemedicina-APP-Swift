5d8050e0776e396785d87c9f3ed5c07fc2e805de
Gabriel P <gpapa.contato@gmail.com>
2020-06-10 19:32:40 -0700

    Initial Commit

---
+++ Telemedicina APP Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telemedicina APP Swift
+//
+//  Created by user on 10/06/20.
+//  Copyright © 2020 Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
